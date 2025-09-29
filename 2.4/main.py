39a326209434d32dad764b232bb0ced7d5f569f5
ErishaRahman <1287178@learn.vsb.bc.ca>
2025-09-29 22:05:00 +0000

    hello

--- 2.4/main.py
+++ 2.4/main.py
@@ -1,6 +1,13 @@
 file = open("2.4/responses.csv")
-
+lineinput = input("whats your name?").lower().strip()
 for line in file:
-    if "erisha" in line.lower():
+    if lineinput in line.lower():
         print(line)
         myline = line
+
+lineinput2 = input("who are you comparing yourself to?").lower().strip()
+for line in file:
+    if lineinput2 in line.lower():
+        print(line)
+        myline = line
+
