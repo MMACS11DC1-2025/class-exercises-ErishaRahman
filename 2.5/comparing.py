13f810a280e4c6fa5c6a69bf9d536b0439e050a3
ErishaRahman <1287178@learn.vsb.bc.ca>
2025-10-01 21:49:25 +0000

    cyka

--- 2.5/comparing.py
+++ 2.5/comparing.py
@@ -8,18 +8,25 @@ Test as you go! Describe in your comments what steps you took to test your code.
 """
 
 file = open("2.4/responses.csv")
-lineinput = input("whats your name?").lower().strip()
+lineinput = input("whats your name?").lower()
 lineinput2 = input("who are you comparing yourself to?").lower().strip()
-
-
+"""
+##fileList = file.read().strip().split("\n")
+line = fileList[22].split(',')
+col1 = line[1]
+print(col1 == line[6])
+"""
 for line in file:
     if lineinput in line.lower():
-        print(line)
-        myline = line
+        lineinput = line.split(",")
+        print("greetings" + lineinput[1] + ".\n")
+       
+
+        """
     if lineinput2 in line.lower():
         print(line)
         myline = line
-
+"""
 
 
 
