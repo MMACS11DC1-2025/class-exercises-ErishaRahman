133d0e5f6acba2e331ebf4d2a3176d05d54e8f71
ErishaRahman <1287178@learn.vsb.bc.ca>
2025-11-12 21:34:55 +0000

    one more game

--- 5.1/image_open.py
+++ 5.1/image_open.py
@@ -1,4 +1,19 @@
-from PIL import Image
+#from PIL import Image
 
-image_green = Image.open("CS11/5.1/kid-green.jpg").load()
-image_beach = Image.open("CS11/5.1/beach.jpg").load()
+#image_green = Image.open("CS11/5.1/kid-green.jpg").load()
+#image_beach = Image.open("CS11/5.1/beach.jpg").load()
+
+def colour(r, g, b):
+    if r <= 24 and g >= 230 and b <= 24:
+        return ("green")
+    if r <= 24 and g <= 24 and b >= 230:
+        return ("blue")
+    if r >= 230 and g <= 24 and b <= 24:
+        return ("red")
+
+
+print(colour(r + g + b))   #green
+print(colour(0, 0, 255))   #blue
+print(colour(255, 0, 0))   #red   
+
+    
