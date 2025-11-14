f7217d56a438462bf7889b2136bd4dcd9aaa709c
ErishaRahman <1287178@learn.vsb.bc.ca>
2025-11-14 21:35:19 +0000

    fried rice chow mein with some sweet and sour sauce

--- 5.1/image_open.py
+++ 5.1/image_open.py
@@ -1,8 +1,9 @@
-#from PIL import Image
+from PIL import Image
+import coolcolours
 
-#image_green = Image.open("CS11/5.1/kid-green.jpg").load()
-#image_beach = Image.open("CS11/5.1/beach.jpg").load()
-
+image_green = Image.open("5.1/kid-green.jpg").load()
+image_beach = Image.open("5.1/beach.jpg").load()
+"""
 def colour(r, g, b):
     if r <= 24 and g >= 230 and b <= 24:
         return ("green")
@@ -15,5 +16,21 @@ def colour(r, g, b):
 print(colour(r + g + b))   #green
 print(colour(0, 0, 255))   #blue
 print(colour(255, 0, 0))   #red   
+"""
 
-    
+image_output = Image.open("5.1/kid-green.jpg")
+
+width = image_output.width
+height = image_output.height
+
+for x in range(width):
+    for y in range(height):
+        r = image_green[x, y][0]
+        g = image_green[x, y][1]
+        b = image_green[x, y][2]
+
+if coolcolours.colour(r, g, b):
+    beach_colour = image_beach[x, y]
+    image_output.putpixel((x, y), beach_colour)
+
+image_output.save("output.png", "png")
