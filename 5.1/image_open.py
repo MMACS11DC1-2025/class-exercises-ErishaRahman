33cd8cc58b3cd6c6d25a2c5935467cb50ce0fdf9
ErishaRahman <1287178@learn.vsb.bc.ca>
2025-11-17 17:06:41 +0000

    alright.

--- 5.1/image_open.py
+++ 5.1/image_open.py
@@ -1,23 +1,11 @@
+
+
 from PIL import Image
 import coolcolours
 
 image_green = Image.open("5.1/kid-green.jpg").load()
 image_beach = Image.open("5.1/beach.jpg").load()
-"""
-def colour(r, g, b):
-    if r <= 24 and g >= 230 and b <= 24:
-        return ("green")
-    if r <= 24 and g <= 24 and b >= 230:
-        return ("blue")
-    if r >= 230 and g <= 24 and b <= 24:
-        return ("red")
-
-
-print(colour(r + g + b))   #green
-print(colour(0, 0, 255))   #blue
-print(colour(255, 0, 0))   #red   
-"""
-
+ 
 image_output = Image.open("5.1/kid-green.jpg")
 
 width = image_output.width
@@ -29,8 +17,9 @@ for x in range(width):
         g = image_green[x, y][1]
         b = image_green[x, y][2]
 
-if coolcolours.colour(r, g, b):
+if coolcolours.colour(r, g, b) == "green":
     beach_colour = image_beach[x, y]
-    image_output.putpixel((x, y), beach_colour)
+    ij = (x,y)
+    image_output.putpixel((ij), beach_colour)
 
 image_output.save("output.png", "png")
