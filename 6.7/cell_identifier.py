0ac92eccd6eff618029a028e630168607058a838
ErishaRahman <1287178@learn.vsb.bc.ca>
2025-12-12 02:54:13 +0000

    oh dear ya ne haco etot

--- 6.7/cell_identifier.py
+++ 6.7/cell_identifier.py
@@ -1,18 +1,17 @@
 from PIL import Image
 
-file1 = Image.open("6.7/astaxanthincells.png.png")
+file1 = Image.open("6.7/astaxanthin.png.png")
 file2 = Image.open("6.7/hepatocytes.jpg.jpg")
 file3 = Image.open("6.7/redbloodcells.jpg.jpg")
-file4 = Image.open("6.7/melanocytes.jpg.jpg")
-file5 = Image.open("6.7/adipocytes.jpg.jpg")
+file4 = Image.open("6.7/melanocyte.jpg.jpg")
+file5 = Image.open("6.7/RetinalRodCells.jpg.jpg")
 file6 = Image.open("6.7/retina.jpg.jpg")
 file7 = Image.open("6.7/hemo.png.png")
-file8 = Image.open("6.7/LutealCells.jpg.jpg")
+file8 = Image.open("6.7/LutealCells.jpg")
 file9 = Image.open("6.7/chromaffin.png")
 file10 = Image.open("6.7/fatcells.jpg.jpg")
 
 files = [file1, file2, file3, file4, file5, file6, file7, file8, file9, file10]
-
 
 def classify_cell(r, g, b):
     if 180 <= r <= 220 and 0 <= g <= 40 and 0 <= b <= 40:
@@ -39,7 +38,9 @@ def classify_cell(r, g, b):
 
 
 for i in range(10):
-
+    '''
+    pix = files[i].load()
+    '''
     yellow_pixels = []
     green_pixels = []
     blue_pixels = []
@@ -51,65 +52,90 @@ for i in range(10):
     yellow_orange_pixels = []
     brown_rich_pixels = []
     pink_pixels = []
-
-    # ✔️ Keep YOUR original width/height code exactly unchanged
     width = files[i].width
     height = files[i].height
 
-    pixels = files[i].load()
+    
 
     for x in range(width):
         for y in range(height):
-            pixel_r = pixels[x, y][0]
-            pixel_g = pixels[x, y][1]
-            pixel_b = pixels[x, y][2]
+            pixel_r = files[i].getpixel((x, y))[0]
+            pixel_g = files[i].getpixel((x, y))[1]
+            pixel_b = files[i].getpixel((x, y))[2]
 
             cell = classify_cell(pixel_r, pixel_g, pixel_b)
 
-            # ✔️ IF STATEMENTS UNCHANGED
+         
             if cell == "yellow":
-                yellow_pixels.append(pixels[x,y])
-                files[i].putpixel((x,y), (255, 255, 255))
+                yellow_pixels.append(files[i].getpixel((x, y)))
+             
 
             if cell == "green":
-                green_pixels.append(pixels[x,y])
-                files[i].putpixel((x,y), (255, 255, 255))
+                green_pixels.append(files[i].getpixel((x, y)))
+            
 
             if cell == "blue":
-                blue_pixels.append(pixels[x,y])
-                files[i].putpixel((x,y), (255, 255, 255))
+                blue_pixels.append(files[i].getpixel((x, y)))
+            
 
             if cell == "red":
-                red_pixels.append(pixels[x,y])
-                files[i].putpixel((x,y), (255, 255, 255))
+                red_pixels.append(files[i].getpixel((x, y)))
+            
 
             if cell == "black":
-                black_pixels.append(pixels[x,y])
-                files[i].putpixel((x,y), (255, 255, 255))
+                black_pixels.append(files[i].getpixel((x, y)))
+            
 
             if cell == "purple":
-                purple_pixels.append(pixels[x,y])
-                files[i].putpixel((x,y), (255, 255, 255))
+                purple_pixels.append(files[i].getpixel((x, y)))
+           
 
             if cell == "brown":
-                brown_pixels.append(pixels[x,y])
-                files[i].putpixel((x,y), (255, 255, 255))
+                brown_pixels.append(files[i].getpixel((x, y)))
+           
 
             if cell == "yellow_orange":
-                yellow_orange_pixels.append(pixels[x,y])
-                files[i].putpixel((x,y), (255, 255, 255))
+                yellow_orange_pixels.append(files[i].getpixel((x, y)))
+              
 
             if cell == "brown_rich":
-                brown_rich_pixels.append(pixels[x,y])
-                files[i].putpixel((x,y), (255, 255, 255))
+                brown_rich_pixels.append(files[i].getpixel((x, y)))
+              
 
             if cell == "pink":
-                pink_pixels.append(pixels[x,y])
-                files[i].putpixel((x,y), (255, 255, 255))
+                pink_pixels.append(files[i].getpixel((x, y)))
+            
+#rid of white cells input that or smth
+num_yellow = len(yellow_pixels)
 
-            if cell == "other":
-                other_pixels.append(pixels[x,y])
-                files[i].putpixel((x,y), (255, 255, 255))
+num_yellow = len(green_pixels)
+
+num_yellow = len(blue_pixels)
+
+num_yellow = len(red_pixels)
+
+num_yellow = len(other_pixels)
+
+total_pixels = width*height
+file.save("output.png", "png")
+
+yellow_ratio = num_yellow / total_pixels
+
+green_ratio = num_yellow / total_pixels
+
+blue_ratio = num_yellow / total_pixels
+
+red_ratio = num_yellow / total_pixels
+
+other_ratio = num_yellow / total_pixels
+
+print(len(yellow_pixels))
+print(len(green_pixels))
+print(len(blue_pixels))
+print(len(red_pixels))
+print(len(other_pixels))
+print(width*height)
+            
 
             
 
